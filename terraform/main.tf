5a987e1d952a08c0cb86478b146868fda7c68d44
nikhilgthakur <nikhilgthakur@outlook.com>
2025-02-21 17:30:22 +0530

    Adding terraform files

---
+++ terraform/main.tf
@@ -0,0 +1,40 @@
+
+# Resource Group
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+# Storage Account
+resource "azurerm_storage_account" "storage" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+# Service Plan
+resource "azurerm_service_plan" "plan" {
+  name                = var.service_plan_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  os_type             = "Linux"
+  sku_name            = var.sku_name
+}
+
+# Linux Function App
+resource "azurerm_linux_function_app" "function" {
+  name                       = var.function_app_name
+  resource_group_name        = azurerm_resource_group.rg.name
+  location                   = azurerm_resource_group.rg.location
+  storage_account_name       = azurerm_storage_account.storage.name
+  storage_account_access_key = azurerm_storage_account.storage.primary_access_key
+  service_plan_id            = azurerm_service_plan.plan.id
+
+  site_config {
+    application_stack {
+      node_version = "18"  
+    }
+  }
+}
---
+++ terraform/provider.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=4.1.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+
+  subscription_id = "a859831e-dbb1-4d73-8b50-a82ac0fcc7d5"
+}
